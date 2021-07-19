f4c4428d12f1687d2da5c0a5e0251dc3b27488f2
jackreynolds <jackreynolds92@gmail.com>
2021-07-19 22:34:19 +1000

    create questions for HTML

---
+++ src/questions/html.js
@@ -0,0 +1,136 @@
+const htmlQuestions = [
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 2,
+    question: "What are tags and attributes in HTML?",
+    answer:
+      "Tags are the primary component of the HTML that defines how the content will be structured/ formatted, whereas Attributes are used along with the HTML tags to define the characteristics of the element. For example, <p align=” center”>Interview questions</p>, in this the ‘align’ is the attribute using which we will align the paragraph to show in the center of the view.",
+  },
+  {
+    number: 3,
+    question: "What are void elements in HTML?",
+    answer:
+      "HTML elements which do not have closing tags or do not need to be closed are Void elements. For Example <br />, <img />, <hr />, etc",
+  },
+  {
+    number: 4,
+    question: "What is the advantage of collapsing white space?",
+    answer:
+      "In HTML, a blank sequence of whitespace characters is treated as a single space character, Because the browser collapses multiple spaces into a single space character and this helps a developer to indent lines of text without worrying about multiple spaces and maintain readability and understandability of HTML codes.",
+  },
+  {
+    number: 5,
+    question: "What are HTML 'Entities'?",
+    answer:
+      "In HTML some characters are reserved like ‘<’, ‘>’, ‘/’, etc. To use these characters in our webpage we need to use the character entities called HTML Entities. Below are a few mapping between the reserved character and its respective entity character to be used.",
+  },
+  {
+    number: 6,
+    question: "What does HTML stand for?",
+    answer: "HTML stands for Hypertext Markup Language",
+  },
+  {
+    number: 7,
+    question: "What are different types of lists in HTML?",
+    answer: "Unordered list <ul></ul> and ordered list <ol></ol>.",
+  },
+  {
+    number: 8,
+    question: "What is the class attribute in HTML?",
+    answer:
+      "The class attribute is used to specify the class name for an HTML element. Multiple elements in HTML can have the same class value. Also, it is mainly used to associate the styles written in the stylesheet with the HTML elements.",
+  },
+  {
+    number: 9,
+    question:
+      "What is the difference between the 'id' attribute and the 'class' attribute of HTML elements?",
+    answer:
+      "Multiple elements in HTML can have the same class value, whereas a value of id attribute of one element cannot be associated with another HTML element.",
+  },
+  {
+    number: 10,
+    question: "Describe HTML layout structure?",
+    answer:
+      "Every webpage has different components to display the intended content and a specific UI. But still, there are a few things which are templated and globally accepted as a way to structure a web page, such as: <header> - <footer> - <nav> - <article> - <section> - <body> - <aside>",
+  },
+  {
+    number: 11,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 12,
+    question: "Define multipart form data",
+    answer:
+      "Multipart form data is one of the values of the enctype attribute. It is used to send the file data to the server-side for processing. The other valid values of the enctype attribute are text/plain and application/x-www-form-urlencoded.",
+  },
+  {
+    number: 13,
+    question: "List some methods on how to optimize web assets loading",
+    answer:
+      "CDN hosting - A CDN is used to geographically distrubute content to help reduce latency. File Compression - helps to reduce the size of an asset. File concatenation - reduce the number of HTTP calls. Minify scripts - reduce the overall file size of JS and CSS files. Parallel downloads - hosting assts in multiple subdomains can help to bypass th edownload limit of 6 assets per domain of all modern browsers. Lazy loading - instead of loading all assets at once, the non-critical assets can be loaded on a need to load basis.",
+  },
+  {
+    number: 14,
+    question: "What is HTML5?",
+    answer:
+      "HTML5 is the latest or updated version of the markup language that defines HTML.",
+  },
+  {
+    number: 15,
+    question:
+      "How can you open a new URL into a new tab when clicking an anchor tag?",
+    answer: "<a href='#' target='_blank'></a>",
+  },
+  {
+    number: 16,
+    question:
+      "What are the various formatting tags in HTML? <b>, <i>, <em>, <big>, <small>, <sub>, <sup>, <del>, <strong>, <mark>, <ins>",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+  {
+    number: 1,
+    question: "Are the HTML tags and elements the same thing?",
+    answer:
+      "No. HTML elements are defined by a starting tag, may contain some content and a closing tag.For example, <h1>Heading 1</h1> is a HTML element but just <h1> is a starting tag and </h1> is a closing tag.",
+  },
+];
+
+export default htmlQuestions;
